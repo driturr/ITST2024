4ee951ccf1bb3833981486936c2c918414740527
driturr <179262840+driturr@users.noreply.github.com>
2024-10-11 20:13:44 +0200

    edited Laboration_1

--- Laboration_1/laboration_1.py
+++ Laboration_1/laboration_1.py
@@ -3,7 +3,6 @@
 #Note 2: If run in Visual Studio Code, option 3 will not work since it requires root privileges.
     #Not recommended to run Visual Code as root. Exposed to vulnerabilities from any extensions within Visual Code
 
-import json
 from nmap_sV import scan as scan_sV
 from nmap_sS import scan as scan_sS
 from nmap_top_ports import scan as scan_top_ports
@@ -12,17 +11,20 @@ from nmap_top_ports import scan as scan_top_ports
 while True:
     ip = input("""********************
 Welcome to the nmap tool!
-What IP will you be scanning today?
+What IP or IPs will you be scanning today?
+(Separate more than one IP using comas ',' between each IP)
         """)    
 
-    print("\n********************\nConfirm IP: \nAre you sure you want to continue with this IP?")
+    ip_list = [ip.strip() for ip in ip.split(",")]
+
+    print("\n********************\nConfirm IPs: \nAre you sure you want to continue?")
     check_ip_correct = input("\nPress 'Y' to continue or any other key to start over: ")
     if check_ip_correct == "Y":
-        break  # Exit the loop if the input is valid
+        break
     else:
             print("\n********************\nRestarting the program...")
 
-print(f"You will scan the IP {ip}")
+print(f"You will scan the IPs: {ip_list}")
 
 print("""\n********************
 Now let's choose what type of scan you want to perform.
@@ -35,27 +37,35 @@ option = input("""
                """)
 
 scan_performed = False
+results = {}
 
 if option == "1":
     selected_option = "Check open ports to determine service/version info"
-    print(f"\nCool! \nYou selected '{selected_option}' on IP {ip} \n************\nScan results for {ip}: \n\a")
-    results = scan_sV(ip)
-    scan_performed = True
-    print(f"\a{results}")
+    print(f"\nCool! \nYou selected '{selected_option}' on IPs: ")
+    for ip in ip_list:
+        print(f"\n\nScanning... {ip}")
+        results[ip] = scan_sV(ip)
+        print(f"\a\n************\nScan results for {ip}:\n\a{results[ip]}")
+        scan_performed = True
+
 
 elif option == "2":
     selected_option = "Scan 100 most common ports"
-    print(f"\nCool! \nYou selected '{selected_option}' on IP {ip} \n************\nScan results for {ip}: \n\a")
-    results = scan_top_ports(ip)
-    scan_performed = True
-    print(results)
+    print(f"\nCool! \nYou selected '{selected_option}' on IPs: ")
+    for ip in ip_list:
+        print(f"\n\nScanning... {ip}")
+        results[ip] = scan_top_ports(ip)
+        print(f"\a\n************\nScan results for {ip}:\n\a{results[ip]}")
+        scan_performed = True
 
 elif option == "3":
     selected_option = "TCP SYN port scan"
-    print(f"\nCool! \nYou selected '{selected_option}' on IP {ip} \n************\nScan results for {ip}: \n\a")
-    results = scan_sS(ip)
-    scan_performed = True
-    print(results)
+    print(f"\nCool! \nYou selected '{selected_option}' on IPs: ")
+    for ip in ip_list:
+        print(f"\n\nScanning... {ip}")
+        results[ip] = scan_sS(ip)
+        print(f"\a\n************\nScan results for {ip}:\n\a{results[ip]}")
+        scan_performed = True
 
 else:
     selected_option = "Invalid option selected"
@@ -67,9 +77,9 @@ if scan_performed == True:
     save_results = input("\n\nPress 'Y' to save the results. Otherwise, press any other key.")
     if save_results == "Y":
         with open("results_nmap.txt", 'w') as file:
-            data = "results_nmap.txt"
             file.write(str(results))
             print("\n\a\aThe results have been saved into 'results_nmap.txt'")
     else:
         print("\nResults not saved. \n\n**************** \nExiting program \n****************")
-    
+else:
+    print("\n No performed scans.") 
